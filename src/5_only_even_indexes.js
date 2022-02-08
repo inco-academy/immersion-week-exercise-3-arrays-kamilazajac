984080e08d9900a471c38d5b128360292314b861
kamilazajac <kamilaagata.zajac@gmail.com>
2022-02-08 10:18:48 +0100

    exercise 5 solved

--- src/5_only_even_indexes.js
+++ src/5_only_even_indexes.js
@@ -1,9 +1,20 @@
 module.exports = evenIndexes;
+function evenIndexes(arr) {
+  let arr2 = [];
+  for (let i = 0; i < arr.length; i += 2) {
+    // take every second element
+    arr2.push(arr[i]);
+  }
+  return arr2;
+}
+evenIndexes('lol');
+evenIndexes("You're weird");
+evenIndexes('');
 
 /**Weryfikacja */
 
 function verify(input, goal) {
-  if (input === goal) {
+  if (input.toString() === goal.toString()) {
     console.log('Gratulacje!');
   } else {
     console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
