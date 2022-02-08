578bbd4437c83a844edd31c90e29d6aa82035cb8
kamilazajac <kamilaagata.zajac@gmail.com>
2022-02-08 11:16:09 +0100

    exercise 6 solved

--- src/6_remove_double.js
+++ src/6_remove_double.js
@@ -1,16 +1,32 @@
-module.exports = function removeDouble() {
-  // TODO Implement me.
-};
+module.exports = removeDouble;
+
+function removeDouble(arr) {
+  const reversedArr = arr.reverse();
+  const result = [...new Set(reversedArr)];
+
+  return result.reverse();
+}
+
+removeDouble(['suit', 'clock', 'butter', 'suit']);
+removeDouble(['hello', 'hello', 23, -3, 23, 'hello']);
 
 /**Weryfikacja */
 
 function verify(input, goal) {
-  if (input === goal) {
+  if (input.toString() === goal.toString()) {
     console.log('Gratulacje!');
   } else {
     console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
   }
 }
 
-verify(removeDouble(["suit", "clock", "butter", "suit"]), [ 'clock', 'butter', 'suit' ]);
-verify(removeDouble(["hello", "hello", 23, -3, 23, "hello"]), [ -3, 23, 'hello' ]);
+verify(removeDouble(['suit', 'clock', 'butter', 'suit']), [
+  'clock',
+  'butter',
+  'suit',
+]);
+verify(removeDouble(['hello', 'hello', 23, -3, 23, 'hello']), [
+  -3,
+  23,
+  'hello',
+]);
