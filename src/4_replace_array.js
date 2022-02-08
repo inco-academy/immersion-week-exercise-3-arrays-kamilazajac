84211cba3aa421d893fedf6457b7f72e7690e95c
kamilazajac <kamilaagata.zajac@gmail.com>
2022-02-08 09:54:57 +0100

    exercise 4 solved

--- src/4_replace_array.js
+++ src/4_replace_array.js
@@ -1,17 +1,39 @@
-module.exports = function replaceArray() {
-  // TODO Implement me.
-};
+module.exports = replaceArray;
+
+function replaceArray(arr) {
+  let arr2 = [];
+  arr.forEach((el) => {
+    let firstLetter = el.substring(0, 1);
+    if (firstLetter === 'h') {
+      wordToUpper = el.toUpperCase();
+      len = arr2.push(wordToUpper);
+    } else if (firstLetter === 'H') {
+      wordToUpper = el.toUpperCase();
+      len = arr2.push(wordToUpper);
+    } else {
+      len = arr2.push(el);
+    }
+  });
+  return arr2;
+}
+replaceArray(['banana', 'suit', 'hammer']);
+replaceArray(['Hello', 'there']);
+replaceArray(['Hey!', 'hey...']);
 
 /**Weryfikacja */
 
 function verify(input, goal) {
-  if (input === goal) {
+  if (input.toString() === goal.toString()) {
     console.log('Gratulacje!');
   } else {
     console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
   }
 }
 
-verify(replaceArray(["banana", "suit", "hammer"]), [ 'banana', 'suit', 'HAMMER' ]);
-verify(replaceArray(["Hello", "there"]), [ 'HELLO', 'there' ]);
-verify(replaceArray(["Hey!", "hey..."]), [ 'HEY!', 'HEY...' ]);
+verify(replaceArray(['banana', 'suit', 'hammer']), [
+  'banana',
+  'suit',
+  'HAMMER',
+]);
+verify(replaceArray(['Hello', 'there']), ['HELLO', 'there']);
+verify(replaceArray(['Hey!', 'hey...']), ['HEY!', 'HEY...']);
